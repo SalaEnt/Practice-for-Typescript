064fa2960ec121a6529ee3688c88a5da4836547d
salar <salarmshaykhy152@gmail.com>
2025-12-25 23:10:25 +0330

    IMP:to know about the ? - ?? - !

--- src/app2.ts
+++ src/app2.ts
@@ -21,3 +21,33 @@ function getUserEmail(user: User): string {
 }
 
 console.log(getUserEmail({profile : {age : 50}} as User));
+
+/*Optional Chaining "?" ==> theres no problem if we dont have anything on our Variables
+and if theres nothing on . we wont recieve any Errors. 
+
+type User = {
+  id: number;
+  profile?: {
+    email?: string;
+    age?: number;
+  };
+};
+*/  
+
+/*Nullish Coalescing "??" ==> if the value of the Varriable was a 'null' | 'undefided'
+replace something on it 
+
+function getUserEmail(user: User): string {
+  return user.profile?.email ?? "no-email";
+}
+*/
+
+/* Non-null Assertion "!" ==>  im sure about it , that the value of the variable is not 'null
+or 'undefided' but if my variable was null i will recieve an Error. 
+
+type User = { profile?: { email?: string } };
+const user: User = { profile: { email: "a@test.com" } };
+
+console.log(user.profile!.email); 
+
+' */
